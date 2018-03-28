0862746c9a782a9a166382c168b878e97b0f65b2
Joel R. Michaliszen <joel.r.mz@gmail.com>
2018-03-27 21:33:54 -0300

    acessando paginas dos cursos udacity

--- courses/courses/spiders/udacity.py
+++ courses/courses/spiders/udacity.py
@@ -12,14 +12,30 @@ class UdacitySpider(scrapy.Spider):
         )
         for div in divs:
             link = div.xpath('.//h3/a')
-            title = link.xpath('./text()').extract_first()
             href = link.xpath('./@href').extract_first()
-            img = div.xpath('.//img[contains(@class, "course-thumb")]/@src'
-                            ).extract_first()
-            descriprion = div.xpath('.//div[2]/span/text()').extract_first()
-            yield {
-                'title': title,
-                'url': href,
-                'img': img,
-                'description': descriprion,
-            }
+            yield scrapy.Request(
+                url='https://br.udacity.com%s' % href,
+                callback=self.parse_detail)
+
+    def parse_detail(self, response):
+        title = response.xpath('//title/text()').extract_first()
+        headline = response.xpath(
+            '//h6[contains(@class, "big hidden-sm-down")]/text()'
+        ).extract_first()
+        instructors = []
+        for div in response.xpath(
+                '/html/body/ir-root/ir-content/ir-ndop-b/section[7]/ir-nd-instructors//div[contains(@class,"card")]'
+        ):
+            instructors.append({
+                'name':
+                div.xpath('./*[contains(@class,"name")]/text()')
+                .extract_first(),
+                ## aguardando SPA
+                #'image':div.xpath('./img/@src').extract_first(),
+            })
+
+        yield {
+            'title': title,
+            'headline': headline,
+            'instructors': instructors,
+        }
