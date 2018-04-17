bdb12f36694696d5493bd1fca30612d4bcea6919
Joel R. Michaliszen <joel.r.mz@gmail.com>
2018-04-16 21:37:08 -0300

    persistindo dados no pipeline item com sqlite 3

--- olx/olx/settings.py
+++ olx/olx/settings.py
@@ -63,9 +63,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'olx.pipelines.OlxPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'olx.pipelines.OlxSqlitePipeline': 1,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
